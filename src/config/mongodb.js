2de34dac950a6c062801edcf13b61f5ff993abd0
Duc Duy <ducduy2222001@gmail.com>
2025-05-04 23:11:59 +0700

    Config server

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,30 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import {MongoClient, ServerApiVersion} from 'mongodb';
+
+const MONGODB_URI ='mongodb+srv://ducduy:nS4ueoPMQMB1UCBu@cluster0.u5c01sd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const DATABASE_NAME = 'kanban-trello-clone-BE'
+
+// khởi tạo kết nối đến MongoDB, ban dầu là null(chưa kết nối)
+let trelloDBInstance = null
+
+// khoi tạo đối tượng để kết nối đến MongoDB
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+});
+
+export const CONNECT_DB = async () => {
+  //goi kết nối đến MongoDB Altas với URI đã khai báo trong mongoClientInstance
+  await mongoClientInstance.connect()
+  // kêt nối thành công thì gán giá trị cho trelloDBInstance
+  trelloDBInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (trelloDBInstance) {
+    return trelloDBInstance
+  }
+  throw new Error('MongoDB is not connected!')
+}
--- src/server.js
+++ src/server.js
@@ -1,32 +1,28 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+/* eslint-disable no-console */
+import express from 'express';
+import { CONNECT_DB, GET_DB } from '~/config/mongodb';
 
-import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+const START_SERVER = async () => {
+  const app = express();
 
-const app = express()
+  const hostname = 'localhost';
+  const port = 8017;
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>');
+  });
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello, I am running at ${hostname}:${port}/`);
+  });
+};
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+CONNECT_DB()
+  .then(() => console.log('Connected to MongoDB'))
+  .then(() => START_SERVER())
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err)
+    process.exit(0)
+  });
