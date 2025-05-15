6430d497d80c70bfccabe918734c8510b76a950e
Duc Duy <ducduy2222001@gmail.com>
2025-05-15 23:51:25 +0700

    add config server and mongodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,30 +1,35 @@
-import {MongoClient, ServerApiVersion} from 'mongodb';
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-const MONGODB_URI ='mongodb+srv://ducduy:nS4ueoPMQMB1UCBu@cluster0.u5c01sd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const DATABASE_NAME = 'kanban-trello-clone-BE'
+const MONGODB_URI =
+  "mongodb+srv://ducduy:nS4ueoPMQMB1UCBu@cluster0.u5c01sd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const DATABASE_NAME = "kanban-trello-clone-BE";
 
 // khởi tạo kết nối đến MongoDB, ban dầu là null(chưa kết nối)
-let trelloDBInstance = null
+let trelloDBInstance = null;
 
 // khoi tạo đối tượng để kết nối đến MongoDB
 const mongoClientInstance = new MongoClient(MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
-    deprecationErrors: true
-  }
+    deprecationErrors: true,
+  },
 });
 
 export const CONNECT_DB = async () => {
   //goi kết nối đến MongoDB Altas với URI đã khai báo trong mongoClientInstance
-  await mongoClientInstance.connect()
+  await mongoClientInstance.connect();
   // kêt nối thành công thì gán giá trị cho trelloDBInstance
-  trelloDBInstance = mongoClientInstance.db(DATABASE_NAME)
-}
+  trelloDBInstance = mongoClientInstance.db(DATABASE_NAME);
+};
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close();
+};
 
 export const GET_DB = () => {
   if (trelloDBInstance) {
-    return trelloDBInstance
+    return trelloDBInstance;
   }
-  throw new Error('MongoDB is not connected!')
-}
+  throw new Error("MongoDB is not connected!");
+};
--- src/server.js
+++ src/server.js
@@ -1,28 +1,37 @@
 /* eslint-disable no-console */
-import express from 'express';
-import { CONNECT_DB, GET_DB } from '~/config/mongodb';
+import express from "express";
+import exitHook from "async-exit-hook";
+import { CLOSE_DB, CONNECT_DB, GET_DB } from "~/config/mongodb";
 
 const START_SERVER = async () => {
   const app = express();
 
-  const hostname = 'localhost';
+  const hostname = "localhost";
   const port = 8017;
 
-  app.get('/', async (req, res) => {
-    console.log(await GET_DB().listCollections().toArray())
-    res.end('<h1>Hello World!</h1><hr>');
+  app.get("/", async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray());
+    res.end("<h1>Hello World!</h1><hr>");
   });
 
   app.listen(port, hostname, () => {
     // eslint-disable-next-line no-console
     console.log(`Hello, I am running at ${hostname}:${port}/`);
   });
+
+  // thư viện async-exit-hook sẽ tự động gọi hàm này khi server dừng lại or Crtl+C hiện tại chỉ hỗ trợ cho MacOS, Linux
+  exitHook(() => {
+    console.log("Closing MongoDB connection...");
+    CLOSE_DB();
+  });
 };
 
-CONNECT_DB()
-  .then(() => console.log('Connected to MongoDB'))
-  .then(() => START_SERVER())
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err)
-    process.exit(0)
-  });
+(async () => {
+  try {
+    await CONNECT_DB();
+    console.log("MongoDB connected successfully!");
+    START_SERVER();
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+})();
